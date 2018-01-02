7229ecba1d3d0e7b882306f6fa33d055e0ba7c3d
Luke Charles <lukecharles222@gmail.com>
2018-01-02 13:40:54 +0000

    question testing implemented

--- app/src/main/java/laminarsoftworks/laminarquiz/Questions.java
+++ app/src/main/java/laminarsoftworks/laminarquiz/Questions.java
@@ -1,8 +1,18 @@
 package laminarsoftworks.laminarquiz;
 
-/**
- * Created by Chill on 02/01/2018.
- */
-
 public class Questions {
+
+    public String mQuestions[] = {
+            "In CS, what is the word for an 8-bit binary value divided by two?",
+            "In CS, what is the word for an 8-bit binary value divided by two?1",
+            "In CS, what is the word for an 8-bit binary value divided by two?2",
+            "In CS, what is the word for an 8-bit binary value divided by two?3",
+            "In CS, what is the word for an 8-bit binary value divided by two?4",
+            "In CS, what is the word for an 8-bit binary value divided by two?5",
+            "In CS, what is the word for an 8-bit binary value divided by two?6",
+            "In CS, what is the word for an 8-bit binary value divided by two?7",
+            "In CS, what is the word for an 8-bit binary value divided by two?8"
+
+    };
+
 }
