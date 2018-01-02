708f2aef5b0bccf250598244839811557cb1c63e
Luke Charles <lukecharles222@gmail.com>
2018-01-02 17:40:20 +0000

    on click listeners implemented

--- app/src/main/java/laminarsoftworks/laminarquiz/MainActivity.java
+++ app/src/main/java/laminarsoftworks/laminarquiz/MainActivity.java
@@ -6,16 +6,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     Button answer1, answer2, answer3, answer4;
 
     TextView score, question;
 
+    private Questions mQuestions = new Questions();
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionsLength = mQuestions.mQuestions.length;
+
+    Random r;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        r = new Random();
 
         answer1 = (Button)findViewById(R.id.answer1);
         answer2 = (Button)findViewById(R.id.answer2);
@@ -24,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
         score = (TextView)findViewById(R.id.score);
         question = (TextView)findViewById(R.id.question);
+
+        updateQuestion(r.nextInt(mQuestionsLength));
 
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,5 +66,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void updateQuestion(int num){
+        question.setText(mQuestions.getQuestion(num));
+        answer1.setText(mQuestions.getChoice1(num));
+        answer2.setText(mQuestions.getChoice2(num));
+        answer3.setText(mQuestions.getChoice3(num));
+        answer4.setText(mQuestions.getChoice4(num));
+
+        mAnswer = mQuestions.getCorrectAnswer(num);
+
+
     }
 }
--- app/src/main/java/laminarsoftworks/laminarquiz/Questions.java
+++ app/src/main/java/laminarsoftworks/laminarquiz/Questions.java
@@ -17,13 +17,45 @@ public class Questions {
 
     private String mChoices[][] = {
             {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"},
-            {"Mercury", "Venus", "Mars", "Saturn"}
+            {"Jupiter", "Venus", "Earth", "Neptune"},
+            {"Earth", "Jupiter", "Pluto", "Venus"},
+            {"Jupiter", "Saturn", "Mars", "Earth"},
+            {"Jupiter", "Pluto", "Mercury", "Earth"},
+            {"Uranus", "Venus", "Mars", "Saturn"},
+            {"Saturn", "Pluto", "Uranus", "Earth"},
+            {"Venus", "Neptune", "Pluto", "Mars"},
+            {"Mercury", "Venus", "Mars", "Pluto"}
     };
+
+    private String mCorrectAnswers[]= {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
+
+    public String getQuestion(int a){
+        String question = mQuestions[a];
+        return question;
+    }
+
+    public String getChoice1(int a){
+        String choice = mChoices[a][0];
+        return choice;
+    }
+
+    public String getChoice2(int a){
+        String choice = mChoices[a][1];
+        return choice;
+    }
+
+    public String getChoice3(int a){
+        String choice = mChoices[a][2];
+        return choice;
+    }
+
+    public String getChoice4(int a){
+        String choice = mChoices[a][3];
+        return choice;
+    }
+
+    public String getCorrectAnswer(int a){
+        String answer = mCorrectAnswers[a];
+        return answer;
+    }
 }
