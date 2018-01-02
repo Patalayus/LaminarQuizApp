6dc6ebe356bc99a99782692427d5df6847cccb45
Luke Charles <lukecharles222@gmail.com>
2018-01-02 17:52:49 +0000

    functions added

--- app/src/main/java/laminarsoftworks/laminarquiz/MainActivity.java
+++ app/src/main/java/laminarsoftworks/laminarquiz/MainActivity.java
@@ -1,5 +1,8 @@
 package laminarsoftworks.laminarquiz;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -37,32 +40,61 @@ public class MainActivity extends AppCompatActivity {
         score = (TextView)findViewById(R.id.score);
         question = (TextView)findViewById(R.id.question);
 
+        score.setText("Score: " + mScore);
         updateQuestion(r.nextInt(mQuestionsLength));
 
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                if(answer1.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestion(r.nextInt(mQuestionsLength));
+                }else{
+                    gameOver();
+                }
 
             }
         });
 
         answer2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                if(answer2.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestion(r.nextInt(mQuestionsLength));
+                }else{
+                    gameOver();
+                }
 
             }
         });
 
         answer3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                if(answer3.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestion(r.nextInt(mQuestionsLength));
+                }else{
+                    gameOver();
+                }
 
             }
         });
 
         answer4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                if(answer4.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestion(r.nextInt(mQuestionsLength));
+                }else{
+                    gameOver();
+                }
 
             }
         });
@@ -77,6 +109,28 @@ public class MainActivity extends AppCompatActivity {
 
         mAnswer = mQuestions.getCorrectAnswer(num);
 
+
+    }
+
+    private void gameOver(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog().Builder(MainActivity.this);
+        alertDialogBuilder
+                .setMessage("Game Over! Your score is: " + mScore + " points.")
+                .setCancelable(false)
+                .setPositiveButton("NEW GAME",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class))
+                            }
+                        })
+                .setNegativeButton("EXIT",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
 
     }
 }
