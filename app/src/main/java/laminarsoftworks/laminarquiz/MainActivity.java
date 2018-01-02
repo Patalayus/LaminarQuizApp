1ed9be2b44c114d2fe5cf822abac4bd70aa128e0
Luke Charles <lukecharles222@gmail.com>
2018-01-02 13:36:42 +0000

    questions.java added

--- app/src/main/java/laminarsoftworks/laminarquiz/MainActivity.java
+++ app/src/main/java/laminarsoftworks/laminarquiz/MainActivity.java
@@ -2,12 +2,55 @@ package laminarsoftworks.laminarquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button answer1, answer2, answer3, answer4;
+
+    TextView score, question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        answer1 = (Button)findViewById(R.id.answer1);
+        answer2 = (Button)findViewById(R.id.answer2);
+        answer3 = (Button)findViewById(R.id.answer3);
+        answer4 = (Button)findViewById(R.id.answer4);
+
+        score = (TextView)findViewById(R.id.score);
+        question = (TextView)findViewById(R.id.question);
+
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        answer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/laminarsoftworks/laminarquiz/Questions.java
@@ -0,0 +1,8 @@
+package laminarsoftworks.laminarquiz;
+
+/**
+ * Created by Chill on 02/01/2018.
+ */
+
+public class Questions {
+}
