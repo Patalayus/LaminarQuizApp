b7f3725c88aa2940c95fbad36272ad1b94fa4c9d
Luke Charles <lukecharles222@gmail.com>
2018-01-16 14:49:22 +0000

    added questions

--- app/src/main/java/laminarsoftworks/laminarquiz/Questions.java
+++ app/src/main/java/laminarsoftworks/laminarquiz/Questions.java
@@ -4,11 +4,11 @@ public class Questions {
 
     public String mQuestions[] = {
             "In CS, what is the word for an 8-bit binary value divided by two?",
-            "In biology what is the powerhouse of the cell?",
-            "What does DNA stand for?",
-            "In CS, what is the word for an 8-bit binary value divided by two?3",
-            "In CS, what is the word for an 8-bit binary value divided by two?4",
-            "In CS, what is the word for an 8-bit binary value divided by two?5",
+            "In biology, what is the powerhouse of the cell?",
+            "In biology, What does DNA stand for?",
+            "In physics, what is Newton's second law?",
+            "In art, who painted the Mona Lisa?",
+            "In biology, who invented penicillin?",
             "In CS, what is the word for an 8-bit binary value divided by two?6",
             "In CS, what is the word for an 8-bit binary value divided by two?7",
             "In CS, what is the word for an 8-bit binary value divided by two?8"
@@ -19,15 +19,15 @@ public class Questions {
             {"A Bit", "A Nibble", "A Float Point", "4-Bit"},
             {"The Nucleus", "The Mitochondria", "The Cell Membrane", "The Cell Wall"},
             {"Detoxin Notrious Anaxyis", "Decrypt of Notrosic Alias", "Deoxyribonucleic Acid", "Description of Neural Analytics"},
-            {"Jupiter", "Saturn", "Mars", "Earth"},
-            {"Jupiter", "Pluto", "Mercury", "Earth"},
-            {"Uranus", "Venus", "Mars", "Saturn"},
+            {"F=MxV", "F=MxA", "P=MxV", "S=D/T"},
+            {"Van Gogh", "Leonardo da Vinci", "Claude Monet", "Banksy"},
+            {"Frank Nightingale", "Charles Darwin", "Albert Einstein", "Alexander Flemming"},
             {"Saturn", "Pluto", "Uranus", "Earth"},
             {"Venus", "Neptune", "Pluto", "Mars"},
             {"Mercury", "Venus", "Mars", "Pluto"}
     };
 
-    private String mCorrectAnswers[]= {"A Nibble", "The Mitochondria", "Deoxyribonucleic Acid", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
+    private String mCorrectAnswers[]= {"A Nibble", "The Mitochondria", "Deoxyribonucleic Acid", "F=MxA", "Leonardo da Vinci", "Alexander Flemming", "Uranus", "Neptune", "Pluto"};
 
     public String getQuestion(int a){
         String question = mQuestions[a];
